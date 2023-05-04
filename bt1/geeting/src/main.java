f49ba7e717249d1ffb1f560354a2c88ee4f4f492
TungSmile <tungmonkey1101@gmail.com>
2023-05-04 23:49:21 +0700

    t9

---
+++ bt1/geeting/src/main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        System.out.println("What u name?");
+        String name=scan.nextLine();
+        System.out.println("Hello: "+name);
+    }
+}
