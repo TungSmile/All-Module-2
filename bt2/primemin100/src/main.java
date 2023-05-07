118ec2c52352ac3a1edf81a5715473df46b22dbf
TungSmile <tungmonkey1101@gmail.com>
2023-05-07 18:58:05 +0700

    t5

---
+++ bt2/primemin100/src/main.java
@@ -0,0 +1,17 @@
+public class main {
+    public static void main(String[] args) {
+        for (int i = 2; i < 101; i++) {
+            boolean check = true;
+            for (int j = 2; j < Math.sqrt(i); j++) {
+                if (i % j == 0) {
+                    check = false;
+                    break;
+                }
+            }
+            if (check) {
+                System.out.println(i);
+            }
+        }
+    }
+}
+
