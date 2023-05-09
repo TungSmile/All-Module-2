ff843dd4dc50a827de9a391d2674da686956a54f
TungSmile <tungmonkey1101@gmail.com>
2023-05-09 08:28:58 +0700

    t5

--- bt3/bt/src/dulicate.java
+++ bt3/bt/src/dulicate.java
@@ -3,19 +3,16 @@ import java.util.Scanner;
 public class dulicate {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("input arr");
+        System.out.print("input arr : ");
         String arr = input.nextLine();
+        System.out.print("input symbol : ");
+        char sym = input.next().charAt(0);
+        int count=0;
         for (int i = 0; i < arr.length(); i++) {
-            int count = 0;
-            for (int j = i; j < arr.length() ; j++) {
-                if (arr.charAt(i) == arr.charAt(j)) {
-                    count++;
-                }
-
-            }
-            if (count > 1) {
-                System.out.println("ký tự " + arr.charAt(i) + " lặp " + count);
+            if (arr.charAt(i)==sym){
+                count++;
             }
         }
+        System.out.println("Symbol: "+sym+" has "+count+" time in string");
     }
 }
